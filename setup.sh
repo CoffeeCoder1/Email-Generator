959e1161f37f0cbf40b2c4700703da12fdcf8b68
Max Nargang <CoffeeCoder1@outlook.com>
2024-05-20 13:46:15 -0400

    Change the setup script to install system site packages Needed so Tkinter works

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
-python3 -m venv "$parent_path/.venv"
+python3 -m venv --system-site-packages "$parent_path/.venv"
 source "$parent_path/.venv/bin/activate"
 pip3 install build
 python3 -m build
