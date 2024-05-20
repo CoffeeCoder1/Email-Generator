38eee696a352ccf7d220228b455694e626e07a82
Max Nargang <CoffeeCoder1@outlook.com>
2024-05-20 13:05:02 -0400

    Change the run script to use a temporary file so the credentials setup flow works

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 source "$parent_path/.venv/bin/activate"
-email-list-parser "$parent_path/data/email-list.txt" | email-generator -t "$parent_path/data/template.json" -
+email-list-parser "$parent_path/data/email-list.txt" > "/tmp/email-list.json"
+email-generator -t "$parent_path/data/template.json" "/tmp/email-list.json"
