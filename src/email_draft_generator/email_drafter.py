f7577aec979874af5366bb25253ccf5be826758d
Max Nargang <CoffeeCoder1@outlook.com>
2024-08-25 16:04:07 -0400

    Make `generate_drafts()` a class method

--- src/email_draft_generator/email_drafter.py
+++ src/email_draft_generator/email_drafter.py
@@ -8,7 +8,8 @@ import concurrent.futures
 class EmailDrafter:
 	"""Utility class to draft E-mails"""
 	
-	def generate_drafts(self, recipients, template: EmailTemplate, creds):
+	@classmethod
+	def generate_drafts(cls, recipients, template: EmailTemplate, creds):
 		with concurrent.futures.ProcessPoolExecutor() as executor:
 			for recipient in recipients:
 				gmail.create_draft(creds, template.create_email_body(EmailRecipient.from_dict(recipient)))
