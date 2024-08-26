128ea8d5091de54a1b24cf259794c94c5e60cee6
Max Nargang <CoffeeCoder1@outlook.com>
2024-08-26 19:13:52 -0400

    Add noconfirm flag to PyInstaller command

--- package.py
+++ package.py
@@ -2,4 +2,4 @@
 
 import PyInstaller.__main__
 
-PyInstaller.__main__.run(['src/email_draft_generator/gui/run.py', '--onedir', '--windowed', '-n', 'E-mail Draft Generator'])
+PyInstaller.__main__.run(['src/email_draft_generator/gui/run.py', '--onedir', '--windowed', '-n', 'E-mail Draft Generator', '--noconfirm'])
