a2ec45fe8b3af8640a70eb32163bfdb70993263a
Max Nargang <CoffeeCoder1@outlook.com>
2024-08-26 22:29:07 -0400

    Include the icon in the package

--- package.py
+++ package.py
@@ -2,4 +2,13 @@
 
 import PyInstaller.__main__
 
-PyInstaller.__main__.run(['src/email_draft_generator/gui/run.py', '--onedir', '--windowed', '-n', 'E-mail Draft Generator', '--noconfirm'])
+PyInstaller.__main__.run([
+    'src/email_draft_generator/gui/run.py',
+    '--onedir',
+    '--windowed',
+    '-n',
+    'E-mail Draft Generator',
+    '--noconfirm',
+    '--icon',
+    'icon.png',
+])
